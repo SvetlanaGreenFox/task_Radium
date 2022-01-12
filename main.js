566cc894948f9be06eff6413a9e4ff144e3f16a2
Светлана <svetlanagreenfox@gmail.com>
2022-01-12 20:35:09 +0300

    Version_1

---
+++ main.js
@@ -0,0 +1,20 @@
+const content = document.querySelector('.card__title');
+
+content.addEventListener('mouseenter', function () {
+  content.classList.add('colorUpdate');
+});
+
+content.addEventListener('mouseleave', function () {
+  content.classList.remove('colorUpdate');
+});
+
+content.addEventListener('click', function () {
+  content.style.opacity = 0;
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.keyCode == '32') {
+    e.preventDefault();
+    content.remove();
+  }
+});
